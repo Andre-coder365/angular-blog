b12af69309843e9afb2acdb7b69928418d3696dd
Andre-coder365 <andreoliveira365@hotmail.com>
2023-05-27 20:55:23 -0300

    conteúdo dinâmico criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO LANÇAMENTO MINECRAFT ANUNCIADO",
+    "description": "Microsoft anuncia novo lançamento do game Minecraft",
+    "photoCover": "https://artcetera.art/wp-content/uploads/2023/02/tudo-sobre-minecraft-1024x576.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Novo game anunciado na Play Store",
+    "description": "bla bla bla bla bla",
+    "photoCover": "https://images.hindustantimes.com/tech/img/2021/06/23/960x540/Google_Play_Store_1624455530234_1624455550430.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Game clássico da Nintendo terá nova versão para Switch",
+    "description": "bla bla bla bla bla",
+    "photoCover": "https://3.bp.blogspot.com/-RaBw3tum_Do/W7KLj5qpxJI/AAAAAAAABFM/punJ-Gv6v-A3d169TBuiKAHQEFcjFxA8QCLcBGAs/s1600/mega_man_11_wallpaper___double_gear_variant_by_seraharcana-dcfcm9x.png"
+  },
+  {
+    "id":"4",
+    "title": "A história dos games: a criação de Space Invaders",
+    "description": "bla bla bla bla bla",
+    "photoCover": "https://i.ytimg.com/vi/k6rTH77tML0/maxresdefault.jpg"
+  }
+]
